b418b2ac09484b64d192bfbd3a699af55dba7396
Nobby Knox <nobbyknox@gmail.com>
2019-11-27 20:44:10 +0200

    Add git credential store and some ls aliases

--- zsh/.aliases.sh
+++ zsh/.aliases.sh
@@ -1,3 +1,7 @@
+alias ll='CLICOLOR_FORCE=1 ls -lh $1 $2 $3 $4 $5 $6 $7 $8 $9 | less -ieFX'
+alias la='CLICOLOR_FORCE=1 ls -lah $1 $2 $3 $4 $5 $6 $7 $8 $9 | less -ieFX'
+alias lll='CLICOLOR_FORCE=1 ls -lah $1 $2 $3 $4 $5 $6 $7 $8 $9 | less -ieFX'
+
 # GIT aliases
 alias git-br='git --no-pager branch'
 alias git-st='git status'
